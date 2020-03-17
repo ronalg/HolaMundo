db40e65c06e19dd503f415c08260e7845d8b653a
ronalg emmanuel quiñones sanchez <quinones_sanchez@outlook.com>
2020-03-17 12:26:08 -0400

    Mantenimiento Unidad

---
+++ Neo/FrmMtoUnidad.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Neo
+{
+    public partial class FrmMtoUnidad : Form
+    {
+        public FrmMtoUnidad()
+        {
+            InitializeComponent();
+        }
+
+        string codigoUnidad = null;
+
+        private void ConfiguraBoton(bool configura)
+        {
+            btnPrimero.Available = configura;
+            btnAnterior.Available = configura;
+            spd1.Available = configura;
+            txtPosicion.Available = configura;
+            lblRegistro.Available = configura;
+            spd2.Available = configura;
+            btnSiguiente.Available = configura;
+            btnUltimo.Available = configura;
+            spd3.Available = configura;
+            btnNuevo.Available = configura;
+            spd4.Available = configura;
+            btnSalir.Available = configura;
+            pnl4.Visible = configura;
+        }
+
+        private void btnNuevo_Click(object sender, EventArgs e)
+        {
+            ConfiguraBoton(false);
+            txtCodigo.Focus();
+            lblTrabajo.Text = Utilidad.codigoTrabajo.ToString();
+            lblEmpresa.Text = Utilidad.codigoTrabajo.ToString();
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.bsMto.EndEdit();
+                if (!btnNuevo.Available)
+                {
+                    taUnidad.Inserta(Utilidad.codigoTrabajo, Utilidad.codigoEmpresa, txtCodigo.Text.Trim(), txtNombre.Text.Trim());
+                    ConfiguraBoton(true);
+                    codigoUnidad = txtCodigo.Text.Trim();
+                }
+                else
+                {
+                    taUnidad.Edita(txtCodigo.Text.Trim(), txtNombre.Text.Trim(), Utilidad.codigoTrabajo, Utilidad.codigoEmpresa, codigoUnidad);
+                }
+            }
+            catch (NoNullAllowedException nullEx)
+            {
+                MessageBox.Show(nullEx.Message, Utilidad.textoCuadroMensaje, MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message, Utilidad.textoCuadroMensaje, MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Utilidad.textoCuadroMensaje, MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Cursor = Cursors.WaitCursor;
+                if (btnNuevo.Available && grdMto.CurrentRow != null)
+                {
+                    DialogResult dr = new DialogResult();
+                    dr = MessageBox.Show(Utilidad.mensajeElimina, Utilidad.textoCuadroMensaje, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1);
+                    if (dr == DialogResult.Yes)
+                    {
+                        taUnidad.Elimina(Utilidad.codigoTrabajo, Utilidad.codigoEmpresa, codigoUnidad);
+                        grdMto.Rows.Remove(grdMto.CurrentRow);
+                    }
+                }
+                else if (grdMto.CurrentRow != null)
+                {
+                    grdMto.Rows.Remove(grdMto.CurrentRow);
+                    ConfiguraBoton(true);
+                }
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message, Utilidad.textoCuadroMensaje, MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, Utilidad.textoCuadroMensaje, MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+            finally
+            {
+                this.Cursor = Cursors.Default;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmMtoUnidad_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Utilidad.mtoUnidad = null;
+        }
+
+        private void grdMto_SelectionChanged(object sender, EventArgs e)
+        {
+            if (grdMto.CurrentRow != null)
+                codigoUnidad = grdMto.CurrentRow.Cells["uCodigo"].Value.ToString();
+        }
+
+        private void FrmMtoUnidad_Load(object sender, EventArgs e)
+        {
+            taUnidad.Fill(dsNeo.tbUnidad, Utilidad.codigoTrabajo, Utilidad.codigoEmpresa);
+        }
+    }
+}
